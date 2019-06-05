51e464d66e21ddbe41d733ac84ca3bf860df8da4
JeraldVictor <jeraldvictor123@yahoo.com>
2019-06-05 09:10:33 +0530

    Jerald's update v1.0.0

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var cors = require('cors')
 
 var indexRouter = require("./routes/index");
 var inventory = require("./routes/inventory");
 
 var app = express();
+
+//enableing cors
+app.use(cors())
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
